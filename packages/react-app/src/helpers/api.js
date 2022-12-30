a54dce286ef2aa87e4c75b939600f19e2bc2c102
Xavier Jouslin <jouslin@nowray.net>
2022-12-30 18:41:46 -0500

    fix: add prev and next condition to fetch

--- packages/react-app/src/helpers/api.js
+++ packages/react-app/src/helpers/api.js
@@ -13,12 +13,11 @@ export function apiRequest({ path, method = "GET", data }) {
 export function getWrapped2019({ ownerAddress, limit, offset }) {
   let path = "https://api.opensea.io/api/v1/assets?asset_contract_address=0x5f53f9f5dcf76757f7cbf35c2e47164c65b9b5ed";
   if (ownerAddress) path += `&owner=${ownerAddress}`;
+  console.log("value of offset is: ", offset);
   if (offset) {
-    path += `&offset=${offset + 1}`;
-    if (limit) path += `&limit=${limit}`;
-  } else {
-    if (limit) path += `&limit=${limit + 1}`;
+    path += `&cursor=${offset}`;
   }
+  if (limit) path += `&limit=${limit}`;
   console.log({ path });
   return apiRequest({ path });
 }
@@ -31,12 +30,10 @@ export function getWrappedCollectionStats() {
 export function getUnwrapped2019({ ownerAddress, limit, offset }) {
   let path = "https://api.opensea.io/api/v1/assets?asset_contract_address=0x34d77a17038491a2a9eaa6e690b7c7cd39fc8392";
   if (ownerAddress) path += `&owner=${ownerAddress}`;
-  if (offset) {
-    path += `&offset=${offset + 1}`;
-    if (limit) path += `&limit=${limit}`;
-  } else {
-    if (limit) path += `&limit=${limit + 1}`;
+  if (offset.next) {
+    path += `&cursor=${offset.next}`;
   }
+  if (limit) path += `&limit=${limit}`;
   console.log({ path });
   return apiRequest({ path });
 }
